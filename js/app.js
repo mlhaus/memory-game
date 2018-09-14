65d8cb7866509d09c5b7d58f23dbd57c38e115b4
Marc Hauschildt <marc.hauschildt@gmail.com>
2018-09-14 09:20:45 -0500

    Load event listener added

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+
+window.addEventListener('load', function(){
+    var iconArray = ['binoculars', 'power', 'headphones', 'fire', 'lifebuoy', 'mug', 'camera', 'bug'];
+    
+    for(var i = 0; i < 16; i++){
+        console.log(i);
+    }
+});
